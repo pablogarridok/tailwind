4de8895897e968eb206cfbba71476246192e9255
pablogarridok <pablogarrub@monlau.com.com>
2025-12-02 17:06:22 +0100

    finiquitado

--- practica_Tailwind/Tamagotchi/script.js
+++ practica_Tailwind/Tamagotchi/script.js
@@ -2,7 +2,7 @@
     let oratoria = 50;
     let energia = 50;
 
-    function actualizarUI() {
+    function actualizarBarras() {
 
       // BARRAS
       document.getElementById("barraintelecto").style.width = intelecto + "%";
@@ -14,7 +14,7 @@
       const img = document.getElementById("personaje");
 
       if (promedio >= 70) img.src = "img/contento.gif";
-      else if (promedio >= 40) img.src = "img/serio.gif";
+      else if (promedio >= 50) img.src = "img/serio.gif";
       else img.src = "img/triste.gif";
     }
 
@@ -22,19 +22,19 @@
       intelecto = Math.min(100, intelecto + 20);
       oratoria = Math.max(0, oratoria - 5);
       energia = Math.max(0, energia - 5);
-      actualizarUI();
+      actualizarBarras();
     }
 
     function debatir() {
       oratoria = Math.min(100, oratoria + 20);
       energia = Math.max(0, energia - 15);
       intelecto = Math.max(0, intelecto - 10);
-      actualizarUI();
+      actualizarBarras();
     }
 
     function meditar() {
       energia = Math.min(100, energia + 30);
       oratoria = Math.max(0, oratoria - 5);
-      actualizarUI();
+      actualizarBarras();
     }
 
