e0c2d96e53ce6ebd3f2be7e6ae25d809604fe342
pablogarridok <pablogarrub@monlau.com.com>
2025-12-02 18:14:36 +0100

    finiquitado

--- practica_Tailwind/Tamagotchi/script.js
+++ practica_Tailwind/Tamagotchi/script.js
@@ -1,16 +1,23 @@
     let intelecto = 50;
     let oratoria = 50;
     let energia = 50;
-
+    
+    let agobiado= 0;
+    
+    
     function actualizarBarras() {
 
-      document.getElementById("barraintelecto").style.width = intelecto + "%";
-      document.getElementById("barraoratoria").style.width = oratoria + "%";
+      document.getElementById("barraIntelecto").style.width = intelecto + "%";
+      document.getElementById("barraOratoria").style.width = oratoria + "%";
       document.getElementById("barraEnergia").style.width = energia + "%";
 
       const promedio = (intelecto + oratoria + energia) / 3;
       const img = document.getElementById("personaje");
 
+      if (agobiado >= 20) {
+        img.src = "img/fumando.gif";
+        return; 
+      }
       if (promedio >= 70) img.src = "img/contento.gif";
       else if (promedio >= 50) img.src = "img/serio.gif";
       else img.src = "img/triste.gif";
@@ -20,6 +27,7 @@
       intelecto = Math.min(100, intelecto + 20);
       oratoria = Math.max(0, oratoria - 5);
       energia = Math.max(0, energia - 5);
+      agobiado++;
       actualizarBarras();
     }
 
@@ -27,12 +35,20 @@
       oratoria = Math.min(100, oratoria + 20);
       energia = Math.max(0, energia - 15);
       intelecto = Math.max(0, intelecto - 10);
+      agobiado=0;
       actualizarBarras();
     }
 
     function meditar() {
       energia = Math.min(100, energia + 30);
       oratoria = Math.max(0, oratoria - 5);
+      agobiado=0;
       actualizarBarras();
     }
 
+    
+
+
+
+
+    
