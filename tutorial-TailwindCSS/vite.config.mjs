74bbaa500540c23d5bc6baede0c868471fc32b35
pablogarridok <pablogarrub@monlau.com.com>
2025-12-02 18:17:03 +0100

    ahora si

--- tutorial-TailwindCSS/vite.config.mjs
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-
-// Configuración de Vite con el plugin oficial de Tailwind v4.1
-export default defineConfig({
-  plugins: [tailwindcss()],
-})
-
